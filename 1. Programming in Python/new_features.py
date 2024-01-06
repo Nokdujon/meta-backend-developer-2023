7f69e6ae0a90e32e343d5f1596ab48f523225408
Nokdujon <nokdujon@outlook.com>
2024-01-07 00:10:34 +0700

    + match case statement

--- 1. Programming in Python/new_features.py
+++ 1. Programming in Python/new_features.py
@@ -1,32 +1,6 @@
 """New Features in Python"""
 
 
-def match_case() -> None:
-    """The Match-Case Statement
-    The match statement In Python was introduced in version 3.10 using
-    the match statement you can achieve cleaner more readable code that allows
-    all the same functionality as the if controlled statement.
-
-    The match statement compares a value to several different conditions
-    until one of these conditions is met.
-    """
-
-    def print_http_status(http_status: int) -> None:
-        """Function printing http sttus."""
-
-        match http_status:
-            case 200 | 201:  # Combining
-                print("Success")
-            case 400:
-                print("Bad request")
-            case 500 | 501:
-                print("Server Error")
-            case _:  # Default if nothing is found in the case
-                print("Unkown status")
-
-        print_http_status(200)
-
-
 def pure_function() -> None:
     """Function A pure function is a function that does not change or
     have any effect on a variable, data, list, or sets beyond its own scope."""
