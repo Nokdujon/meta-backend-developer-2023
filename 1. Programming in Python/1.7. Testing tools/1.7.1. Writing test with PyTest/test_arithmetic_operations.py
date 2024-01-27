50a574b10dfc602157722c4eef1caf87feb40898
Nokdujon <nokdujon@outlook.com>
2024-01-28 06:35:40 +0700

    + doc string for test code

--- 1. Programming in Python/1.7. Testing tools/1.7.1. Writing test with PyTest/test_arithmetic_operations.py
+++ 1. Programming in Python/1.7. Testing tools/1.7.1. Writing test with PyTest/test_arithmetic_operations.py
@@ -5,16 +5,20 @@ import arithmetic_operations
 
 
 def test_add():
+    """_Function test code for add()_"""
     assert arithmetic_operations.add(4, 5) == 9
 
 
 def test_sub():
+    """_Function test code for sub()_"""
     assert arithmetic_operations.sub(4, 5) == -1
 
 
 def test_mul():
+    """_Function test code for mul()_"""
     assert arithmetic_operations.mul(4, 5) == 20
 
 
 def test_div():
+    """_Function test code for div()_"""
     assert arithmetic_operations.div(4, 5) == 0.8
